fc29f025c29d77a8bf9ce432baf665ecb37a83f7
Nishit Patel <nishitpatel.dev@gmail.com>
2025-02-02 09:34:55 +0530

    feat: add Arcjet shield and bot protection
    - Integrate Arcjet for enhanced security with shield and bot detection.
    - Block malicious traffic and unwanted bots to reduce server load.
    - Allow legitimate bots (e.g., search engines, Inngest) for seamless functionality.

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
@@ -6,7 +7,26 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-export default clerkMiddleware(async (auth, req) => {
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
+  rules: [
+    // Shield protection for content and security
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
+
+const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
@@ -15,6 +35,8 @@ export default clerkMiddleware(async (auth, req) => {
     return redirectToSignIn();
   }
 });
+
+export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
